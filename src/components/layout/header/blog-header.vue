6966ca3c2e60ca96f4640db769f191beb35a9112
zzj <www1121c@126.com>
2024-12-24 18:01:38 +0800

    t

--- src/components/layout/header/blog-header.vue
+++ src/components/layout/header/blog-header.vue
@@ -1,13 +1,60 @@
 <script setup lang="ts">
 
-console.log("blog-header");
+import { onMounted, onBeforeUnmount, computed, reactive, } from 'vue';
+import { debounce } from "@/utils/tool";
+const headerState = reactive({
+    drawerShow: false,
+    startScrollTop: 0,
+    headerClass: "",
+    activeIndex: 0,
+    scrollTop: 0,
+});
 
+// 顶部导航固定
+const scroll = debounce(() => {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const { startScrollTop } = headerState;
+    headerState.scrollTop = scrollTop;
+    if (scrollTop <= 50) {
+        headerState.headerClass = "fixed-header";
+        headerState.startScrollTop = scrollTop;
+        return;
+    }
+    if (startScrollTop > scrollTop) {
+        headerState.headerClass = "fixed-header";
+    } else if (startScrollTop <= scrollTop) {
+        headerState.headerClass = "hide-header";
+    }
+    headerState.startScrollTop = scrollTop;
+
+    // console.log("scrollTop", scrollTop, startScrollTop);
+}, 5);
+
+onMounted(() => {
+    // 页面增加滚动事件  
+    window.addEventListener("scroll", scroll);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener("scroll", scroll);
+});
 
 </script>
 
 <template>
-    <div>
-        header
+    <div class="header" :style="{
+        background: headerState.scrollTop < 50 ? 'transparent' : 'var(--header-bg)',
+    }">
+        header11
     </div>
 </template>
 
+<style lang="scss" scoped>
+.header {}
+
+.fixed-header {
+  animation-name: header;
+  animation-duration: 0.8s;
+  animation-fill-mode: forwards;
+}
+</style>
