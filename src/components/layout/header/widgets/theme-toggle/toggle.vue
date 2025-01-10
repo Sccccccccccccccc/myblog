42ca7a96cb9b97473459c86e12b0957fbf12620f
zzj <www1121c@126.com>
2025-01-10 17:54:47 +0800

    feat: 主题切换

--- src/components/layout/header/widgets/theme-toggle/toggle.vue
+++ src/components/layout/header/widgets/theme-toggle/toggle.vue
@@ -1,12 +1,43 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { Moon, Sunrise } from "@element-plus/icons-vue";
+import { storeToRefs } from "pinia";
 
-const currentTheme = ref(true)
+import { useThemeSwitchStore } from '@/store/themeSwitch'
+
+const themeSwitchStore = useThemeSwitchStore()
+const { mainTheme } = storeToRefs(themeSwitchStore);
+
+const currentTheme = ref("")
 
 const changeSwitch = () => {
+    themeSwitchStore.switchMainTheme()
     console.log("changeTheme");
 };
+
+onMounted(() => {
+    const theme = localStorage.getItem("mainTheme");
+    // 若存在缓存用缓存
+    if (theme) {
+        const currentTheme = mainTheme.value ? "dark" : "light";
+        if (currentTheme !== theme) {
+            changeSwitch()
+        }
+    } else {
+        const now = new Date().getHours();
+        console.log("现在是：" + now);
+        if (now >= 6 && now <= 18) {
+            changeSwitch()
+        } else {
+            if (!mainTheme.value) {
+                changeSwitch();
+            }
+        }
+    }
+    currentTheme.value = mainTheme.value;
+
+})
+
 </script>
 
 <template>
